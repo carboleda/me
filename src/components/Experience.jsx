c2ab16256bf519a750d32d5dd6bcef15efa29367
Carlos Fernando Arboleda Garces <arbofercho@gmail.com>
2019-08-20 18:44:17 -0500

    Estilos

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import H2Styled from '../styled/H2Styled';
+import H3Styled from '../styled/H3Styled';
+import PStyled from '../styled/PStyled';
 
 const Experience = props => (
     <div className="Experience">
         <div className="Experience-container">
+            <H2Styled name="Experience" />
             {
                 props.data.map((exp, index) => (
                     <div className="Experience-item" key={`Exp-${index}`}>
-                        <h3>
+                        <H3Styled>
                             {exp.jobTitle} @ {exp.company}
                             <span>{exp.startDate} - {exp.endDate}</span>
-                        </h3>
-                        <p>{exp.jobDescription}</p>
+                        </H3Styled>
+                        <PStyled name={exp.jobDescription} />
                     </div>
                 ))
             }
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
 import Main from '../components/Main';
 import Sidebar from '../components/Sidebar';
 import Info from '../components/Info';
@@ -9,16 +10,26 @@ import Certificates from '../components/Certificates';
 import Skills from '../components/Skills';
 import useGetData from '../hooks/useGetData';
 
+const GlobalStyle = createGlobalStyle`
+    body {
+        font-family: 'Lato' sans-serif;
+        margin: 0;
+        padding: 0;
+        background: #F5F5F5;
+    }
+`;
+
 const App = () => {
     const data = useGetData();
     return data.length === 0 ? <h1>Cargando...</h1> : (
         <Main>
+            <GlobalStyle />
             <Sidebar>
                 <About
                     avatar={data.avatar}
                     name={data.name}
                     profession={data.profession}
-                    bio={data.dio}
+                    bio={data.bio}
                     address={data.address}
                     social={data.social} />
             </Sidebar>
