c7b93c36d836d698b6da1e1e57b732f7e212a6f9
Carlos Fernando Arboleda Garces <arbofercho@gmail.com>
2019-09-02 22:46:35 -0500

    Fix datasource

--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 
 //const API = 'https://us-central1-gndx-cv.cloudfunctions.net/me';
-//const API = 'https://us-central1-test-1-600e1.cloudfunctions.net/me';
-const API = 'public/me.json';
+const API = 'https://us-central1-test-1-600e1.cloudfunctions.net/me';
+//const API = 'public/me.json';
 
 const useGetData = () => {
     const [ myData, setData ] = useState([]);
